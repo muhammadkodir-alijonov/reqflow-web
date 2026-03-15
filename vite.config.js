32b1bd58ae9238570abbb4a15b09e0b49d774eeb
muhammadqodir.a <muhammadqodir.alijonov@fido.uz>
2026-03-15 16:03:07 +0500

    feat: update Vite server and preview configurations to use port 6061

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: "0.0.0.0",
-    port: 6000,
+    port: 6061,
     strictPort: true
   },
   preview: {
     host: "0.0.0.0",
-    port: 6000,
+    port: 6061,
     strictPort: true
   }
 });
